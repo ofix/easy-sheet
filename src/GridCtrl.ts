0d0ffef12297fbc918573534ab68a4abd76ac21a
codelighter <shb8845369@gmail.com>
2017-12-31 13:57:48 +0800

    add DrawInCache

--- src/GridCtrl.ts
+++ src/GridCtrl.ts
@@ -31,6 +31,11 @@ namespace EasySheet{
         protected _parent:CView;
         protected _scrollX:number;
         protected _scrollY:number;
+        protected _cacheExist:boolean;
+        protected _vx:number;
+        protected _vy:number;
+        protected _vw:number;
+        protected _vh:number;
         protected _ctx:CanvasRenderingContext2D;
         protected _cacheCanvas:HTMLCanvasElement;
         protected _cacheCtx:CanvasRenderingContext2D;
@@ -44,8 +49,13 @@ namespace EasySheet{
             this._y = CELL_HEIGHT;
             this._w = nCols * CELL_WIDTH;
             this._h = nRows * CELL_HEIGHT;
+            this._vx = this._x;
+            this._vy = this._y;
+            this._vw = this._w;
+            this._vh = this._h;
             this._rows = [];
             this._cols = [];
+            this._cacheExist = false;
             for(let i=0; i<nRows;i++){
                 this._rows.push(CELL_HEIGHT);
             }
@@ -53,7 +63,7 @@ namespace EasySheet{
                 this._cols.push(CELL_WIDTH);
             }
             this._ctx = this._parent.context;
-            this.CreateCacheRender();
+            this.CreateCacheCtx();
         }
         get clientWidth():number{
             return this._parent.clientWidth;
@@ -61,7 +71,7 @@ namespace EasySheet{
         get clientHeight():number{
             return this._parent.clientHeight;
         }
-        CreateCacheRender():void{
+        CreateCacheCtx():void{
             this._cacheCanvas = document.createElement("canvas");
             this._cacheCanvas.width = this._w;
             this._cacheCanvas.height = this._h;
@@ -106,41 +116,50 @@ namespace EasySheet{
             }
         }
         Draw():void{
-            //画横线
-            this._ctx.save();
-            this._ctx.fillStyle = "#FFF";
-            this._ctx.fillRect(this._x,this._y,this._w,this._h);
-            this._ctx.strokeStyle="#C5C5C5";
+            if(!this._cacheExist){
+                this.DrawInCache();
+            }
+            this._ctx.drawImage(this._cacheCanvas,this._vx,this._vy,this._vw,this._vh);
+        }
+        DrawInCache():void{
+            //清空左侧行
+            this._cacheCtx.save();
+            this._cacheCtx.fillStyle = "#FFF";
+            this._cacheCtx.fillRect(this._x,this._y,this._w,this._h);
+            this._cacheCtx.strokeStyle="#C5C5C5";
+            //画背景横线
+            this._cacheCtx.beginPath();
             let yOffset:number = this._y;
             for(let i=0;i<this._nRows;i++){
-                this._ctx.beginPath();
-                this._ctx.moveTo(this._x,yOffset+this._rows[i]);
-                this._ctx.lineTo(this._x+this._w,yOffset+this._rows[i]);
+                this._cacheCtx.moveTo(this._x,yOffset+this._rows[i]);
+                this._cacheCtx.lineTo(this._x+this._w,yOffset+this._rows[i]);
                 yOffset += this._rows[i];
-                this._ctx.closePath();
-                this._ctx.stroke();
             }
+            this._cacheCtx.stroke();
+            //画背景竖线
+            this._cacheCtx.beginPath();
             let xOffset:number=this._x;
             for(let j=0;j<this._nCols;j++){
-                this._ctx.beginPath();
-                this._ctx.moveTo(xOffset+this._cols[j],this._y);
-                this._ctx.lineTo(xOffset+this._cols[j],this._y+this._h);
+                this._cacheCtx.moveTo(xOffset+this._cols[j],this._y);
+                this._cacheCtx.lineTo(xOffset+this._cols[j],this._y+this._h);
                 xOffset += this._cols[j];
-                this._ctx.stroke();
             }
-
-            this._ctx.font = DEFAULT_FONT_SIZE + 'px ' + "Arial";
-            this._ctx.textBaseline="middle";
-            this._ctx.textAlign = "center";
-            this._ctx.fillStyle = "#000";
+            this._cacheCtx.stroke();
+            this._cacheCtx.beginPath();
+            //画Cell单元格
+            this._cacheCtx.font = DEFAULT_FONT_SIZE + 'px ' + "Arial";
+            this._cacheCtx.textBaseline="middle";
+            this._cacheCtx.textAlign = "center";
+            this._cacheCtx.fillStyle = "#000";
             for(let i=0; i<this._nRows; i++){
                 for(let j=0; j<this._nCols; j++){
                     let xy = this.GetItemXY(i,j);
-                    this._ctx.fillText(""+i+j,xy.x+CELL_WIDTH/2,xy.y+CELL_HEIGHT/2);
-                    this._ctx.stroke();
+                    this._cacheCtx.fillText(""+i+j,xy.x+CELL_WIDTH/2,xy.y+CELL_HEIGHT/2);
                 }
             }
-            this._ctx.restore();
+            this._cacheCtx.stroke();
+            this._cacheCtx.restore();
+            this._cacheExist = true;
         }
     }
 }
